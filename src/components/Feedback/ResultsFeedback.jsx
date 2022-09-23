f3fc7f13debefafedc489302f500dd763d09d9e3
Nataly <97055353+Nataliya1508@users.noreply.github.com>
2022-09-23 22:57:03 +0300

    .

--- src/components/Feedback/ResultsFeedback.jsx
+++ src/components/Feedback/ResultsFeedback.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from "prop-types";
-import styles from './feedback.module.css';
+// import styles from './feedback.module.css';
 
 
 export default function ResultsFeedback({ good, neutral, bad, total,positivePercent }) {

14fde45eba26086dcb0749185c92a76f97798270
Nataly <97055353+Nataliya1508@users.noreply.github.com>
2022-09-26 23:40:29 +0300

    ...

--- src/components/Feedback/ActionsFeedback.jsx
+++ src/components/Feedback/ActionsFeedback.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import PropTypes from "prop-types";
 
-export default function ActionsFeedback({leaveFeedback}) {
+export default function ActionsFeedback({options, leaveFeedback}) {
   return (
     <>
-    <div>
-        <button onClick={() => leaveFeedback("good")}>Good</button>
+      <div>
+        {options.map(option => {
+          const name = option[0] + option.slice(1);
+          return <button key={option} type='button' onClick={() => leaveFeedback(option)}>{name}</button>
+        })}
+        {/* <button onClick={() => leaveFeedback("good")}>Good</button>
   
     
         <button onClick={() => leaveFeedback("neutral")}>Neutral</button>
           
           
-        <button onClick={() => leaveFeedback("bad")}>Bad</button>
+        <button onClick={() => leaveFeedback("bad")}>Bad</button> */}
     </div>
 </>
   )
